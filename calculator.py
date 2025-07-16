4af4f2094bed4cb939703d0602414c300da3c908
muhammadnaeen262@gmail.com <muhammadnaeen262@gmail.com>
2025-07-16 07:30:57 -0400

    update the k8s/deployment

--- calculator.py
+++ calculator.py
@@ -11,9 +11,10 @@ def add():
     except Exception as e:
         return jsonify(error=str(e)), 400
 
-@app.route("/health")
+@app.route("/healthz")
 def health():
-    return jsonify(status="okay")
+    return jsonify({"status": "ok"}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
